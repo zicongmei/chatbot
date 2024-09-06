52e3e9e57162e4ee85bfbff57c3503f8835092cb
zicongmei <zicongmei@gmail.com>
2024-09-06 22:03:23 +0000

    change meta

--- meta.py
+++ meta.py
@@ -1,22 +1,53 @@
-import transformers
+# Load model directly
+from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
+from pathlib import Path
 
-model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
-    device_map="auto",
-)
+model_name = "meta-llama/Llama-2-7b-chat-hf"
+device = "cuda"
 
-messages = [
-    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
-    {"role": "user", "content": "Who are you?"},
-]
+access_token = Path("token").read_text().strip()
 
-outputs = pipeline(
-    messages,
-    max_new_tokens=256,
-)
-print(outputs[0]["generated_text"][-1])
+tokenizer = AutoTokenizer.from_pretrained(model_name,
+                                          trust_remote_code=True,
+                                          token=access_token)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype=torch.bfloat16,
+    device_map=device,
+    trust_remote_code=True,
+    token=access_token)
+
+print("loaded model.")
+
+conversation_history = []
+
+while True:
+    # Get the input data from the user
+    input_text = input("> ")
+
+    conversation_history.append({"role": "user", "content": input_text})
+
+    # Tokenize the input text and history
+    model_inputs = tokenizer.apply_chat_template(conversation_history,
+                                                 return_tensors="pt",
+                                                 add_generation_prompt=False).to(device)
+
+    # Generate the response from the model
+    outputs = model.generate(model_inputs,
+                             max_new_tokens=1024,
+                             top_p=0.7,
+                             temperature=0.7,
+                             pad_token_id=tokenizer.eos_token_id)
+
+    # Decode the response
+    output_token_ids = [
+        outputs[i][len(model_inputs[i]):] for i in range(len(model_inputs))
+    ]
+
+    responses = tokenizer.batch_decode(
+        output_token_ids, skip_special_tokens=True)[0]
+    print("== ", responses)
+    conversation_history.append({"role": "bot", "content": responses})
+
